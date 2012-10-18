91849ad689bcdb2dd12dfca127c11e1b1dec2eb5
Tim Morgan <tim@squareup.com>
2012-10-17 22:48:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+Squash Uploader Library
+=======================
+
+This shared code is used by the various Squash gems to handle communicating with
+a Squash server.
+
+Documentation
+-------------
+
+Comprehensive documentation is written in YARD- and Markdown-formatted comments
+throughout the source. To view this documentation as an HTML site, run
+`rake doc`.
+
+For an overview of the various components of Squash, see the website
+documentation at https://github.com/SquareSquash/web.
+
+Compatibility
+-------------
+
+This library is compatible with Ruby 1.8.6 and later, including Ruby Enterprise
+Edition.
+
+Requirements
+------------
+
+The only dependency is the `json` gem (http://rubygems.org/gems/json). You can
+use any JSON gem that conforms to the typical standard (`require 'json';
+object.to_json`).
+
+Usage
+-----
+
+This library is used internally by the various Squash gems. If you feel like you
+can make use of its code in your project, consult the class documentation for
+more information.
---
+++ lib/squash/uploader.rb
@@ -0,0 +1,110 @@
+# Copyright 2012 Square Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+require 'net/https'
+require 'json'
+
+# Container module for classes relating to Squash.
+
+module Squash
+
+  # Class that handles communicating with the Squash API.
+  #
+  # By default, transmission is done with `Net::HTTP`. If this is unpalatable to
+  # you, reimplement {#http_post}.
+
+  class Uploader
+
+    # Default configuration options. See {#initialize}.
+    DEFAULT_CONFIGURATION = {
+        :open_timeout      => 15,
+        :read_timeout      => 15,
+        :skip_verification => false,
+        :success           => [Net::HTTPSuccess]
+    }
+
+    # @return [String] The host name and scheme of the Squash server (e.g.,
+    #   "https://squash.mycompany.com").
+    attr_accessor :host
+    # @return [Hash<Symbol, Object>] Additional options for uploading.
+    attr_accessor :options
+
+    # Creates a new Squash uploader that will communicate with a given host.
+    #
+    # @param [String] host The host name and scheme of the Squash server (e.g.,
+    #   "https://squash.mycompany.com").
+    # @param [Hash] options Additional options.
+    # @option options [Fixnum] :open_timeout (15) The number of seconds to wait
+    #   when opening a connection to the Squash server.
+    # @option options [Fixnum] :read_timeout (15) The number of seconds to wait
+    #   when waiting for data from the Squash server.
+    # @option options [true, false] :skip_verification (false) If `true`, SSL peer
+    #   verification will not be performed.
+    # @option options [Array<Class>] :success ([Net::HTTPSuccess]) A list of
+    #   subclasses of `Net::HTTPResponse` that are considered successful and will
+    #   not raise an exception.
+
+    def initialize(host, options={})
+      @host    = host
+      @options = DEFAULT_CONFIGURATION.merge(options)
+    end
+
+    # Transmits information to Squash.
+    #
+    # @param [String] path The path portion of the URL, with leading slash.
+    # @param [Hash] data Data to JSON-serialize and place in the request body.
+
+    def transmit(path, data)
+      http_post (host + path),
+                {'Content-Type' => 'application/json'},
+                [data.to_json]
+    end
+
+    protected
+
+    # Override this method to use your favorite HTTP library. This method receives
+    # an array of bodies. It is intended that each element of the array be
+    # transmitted as a separate POST request, _not_ that the bodies be
+    # concatenated and sent as one request.
+    #
+    # A response of code found in the `:success` option is considered successful.
+    #
+    # @param [String] url The URL to POST to.
+    # @param [Hash<String, String>] headers The request headers.
+    # @param [Array<String>] bodies The bodies of each request to POST.
+    # @raise [StandardError] If a response other than 2xx or 422 is returned.
+
+    def http_post(url, headers, bodies)
+      uri               = URI.parse(url)
+      http              = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl      = (uri.scheme == 'https')
+      http.open_timeout = options[:open_timeout]
+      http.read_timeout = options[:read_timeout]
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE if options[:skip_verification]
+
+      http.start do |session|
+        bodies.each do |body|
+          request = Net::HTTP::Post.new(uri.request_uri)
+          headers.each { |k, v| request.add_field k, v }
+          request.body = body
+          response     = session.request(request)
+
+          if options[:success].none? { |cl| response.kind_of?(cl) }
+            raise "Unexpected response from Squash host: #{response.code}"
+          end
+        end
+      end
+    end
+  end
+end
